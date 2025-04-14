0fb7b25f7b280c8b5bfde1d7ea9712b36ab3a554
HaroonAlQasmi <162595919+HaroonAlQasmi@users.noreply.github.com>
2025-04-14 15:28:54 +0400

    Task/86et4znt8 java script practice ii (#2)
    * Made an evenOdd number checker
    
    * made a sum function
    
    * Made fizzBuzz Function
    
    * fix
    
    * made a function to find the largest number
    
    * made a vowel counting function
    
    * made an html file
    
    * fixes
    
    * fixed some stuff
    
    * Finished the first practice task
    
    * Made An array reverser
    
    * made a Positive numbers filter
    
    * Made an array sum function
    
    * Finished the task by making a word counting function

---
+++ JSP2/PracticeJS2.js
@@ -0,0 +1,43 @@
+function arrayReverser() {
+    let userInput = prompt("Enter an Array seperate objects by ',':");
+    let userArray = userInput.split(",");
+    let reversedArray = [];
+    for (let i = 1; i <= userArray.length; i++) {
+        reversedArray.push(userArray.at(-i));
+    }
+    alert("The Array in reverse is "+ reversedArray)
+}
+function positiveFilter() {
+    let userInput = prompt("Enter numbers to filter out the negatives, Seperate them by ',':");
+    let userArray = userInput.split(",").map(Number);
+    let positiveArray = [];
+    for(let i = 0; i < userArray.length; i++){
+        if (userArray[i] > 0){
+            positiveArray.push(userArray[i]);
+        }
+    }
+    alert("The positive numbers are: " + positiveArray);
+}
+function arraySum() {
+    let userInput = prompt("Enter numbers and seperate them using ',':");
+    let userArray = userInput.split(",").map(Number);
+    let sum = 0;
+    for(let i = 0; i < userArray.length;i++){
+        sum += userArray[i];
+    }
+    alert("The sum of the array is: "+sum);
+}
+function wordCounter() {
+    let userInput = prompt("Enter a sentance:");
+    let userArray = userInput.split(" ");
+    let wordCount = {};
+    for(let i = 0; i < userArray.length;i++){
+        if (userArray[i] in wordCount){
+            wordCount[userArray[i]] += 1;
+        }
+        else{
+            wordCount[userArray[i]] = 1;
+        }
+    }
+    alert("The Words count is: "+JSON.stringify(wordCount));
+}
