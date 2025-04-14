bfefffe0b7da4afe43620f76b92af87cd3a7e574
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-14 12:05:58 +0400

    fixes

--- JSP1/PracticeJS1.js
+++ JSP1/PracticeJS1.js
@@ -14,7 +14,7 @@ function sumOfNum (){
     let userInput = parseInt(prompt("Enter a number to get the sum:"));
     let sum = 0;
     for (let i = 0; i <= userInput; i++){
-        sum =+ i;
+        sum += i;
     }
     alert("The sum of "+userInput+" is "+sum)
 }
@@ -23,18 +23,18 @@ function fizzBuzz (){
     let buzzCount = null;
     let fizzBuzzCount = null;
     let nNumCount = null;
-    for (let i=1; i > 100 ; i++){
+    for (let i=1; i <= 100 ; i++){
         if (i % 3 == 0){
-            fizzCount =+ " Fizz ";
+            fizzCount += " Fizz ";
         }
         else if (i % 5 == 0){
-            buzzCount =+ " Buzz ";
+            buzzCount += " Buzz ";
         }
-        else if (i % 3 == 0 & i % 5 == 0){
-            fizzBuzzCount =+ " FizzBuzz ";
+        else if (i % 3 == 0 && i % 5 == 0){
+            fizzBuzzCount += " FizzBuzz ";
         } 
         else{
-            nNumCount=+(string(i)+",");
+            nNumCount+=(i.toString+",");
         }
     }
     alert(fizzCount + buzzCount + fizzBuzzCount + nNumCount);
@@ -42,8 +42,8 @@ function fizzBuzz (){
 function findLargestNum (){
     let userInput = prompt("Enter numbers seperated by ',':");
     let userArray = userInput.split(",").map(num => parseFloat(num.trim()));
-    for (let i = 0; i > userArray.length;i++){
-        let currentLargestNumber = 0;
+    let currentLargestNumber = 0;
+    for (let i = 0; i < userArray.length;i++){
         if (i > currentLargestNumber){
             currentLargestNumber = i;
         }
@@ -54,8 +54,8 @@ function vowelCounter(){
     let userInput = prompt("Enter a string to check for vowels: ");
     for (let i = 0; i > userInput.length;i++){
         let vowelCount = 0;
-        if (userInput[i].toLowerCase in ["a","e","i","o","u"]){
-            vowelCount =+ 1;
+        if (["a", "e", "i", "o", "u"].includes(userInput[i].toLowerCase())){
+            vowelCount += 1;
         }
     }
     alert("The vowel count is "+vowelCount);
