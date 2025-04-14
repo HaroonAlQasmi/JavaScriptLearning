5f2569a8cf5d0b9d753133544d076a061ebd3527
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-14 15:02:49 +0400

    made a Positive numbers filter

--- JSP2/PracticeJS2.js
+++ JSP2/PracticeJS2.js
@@ -1,4 +1,4 @@
-function arrayReverser (){
+function arrayReverser() {
     let userInput = prompt("Enter an Array seperate objects by ',':");
     let userArray = userInput.split(",");
     let reversedArray = [];
@@ -6,4 +6,15 @@ function arrayReverser (){
         reversedArray.push(userArray.at(-i));
     }
     alert("The Array in reverse is "+ reversedArray)
+}
+function positiveFilter() {
+    let userInput = prompt("Enter numbers to filter out the negatives, Seperate them by ',':");
+    let userArray = userInput.split(",").map(Number);
+    let positiveArray = [];
+    for(let i = 0; i < userArray.length; i++){
+        if (userArray[i] > 0){
+            positiveArray.push(userArray[i]);
+        }
+    }
+    alert("The positive numbers are: " + positiveArray);
 }
