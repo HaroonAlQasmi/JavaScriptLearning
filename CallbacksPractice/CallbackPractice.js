a9edd77762305ec742f83e3f285faf046ed1a08b
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-15 09:21:55 +0400

    Made a calculating function

--- CallbacksPractice/CallbackPractice.js
+++ CallbacksPractice/CallbackPractice.js
@@ -2,6 +2,21 @@ function greet(name,callback){
     console.log(`Hello, ${name}!`);
     callback();
 }
-function calculate(a,b,operation){
-
+function calculate(a,operation,b){
+    let answer = 0;
+    switch (operation) {
+        case 'add':
+            answer = a + b;
+            break;
+        case 'subtract':
+            answer = a - b;
+            break;
+        case 'multiply':
+            answer = a * b;
+            break;
+        case 'division':
+            answer = a / b;
+            break;
+    }
+    console.log(answer);
 }
