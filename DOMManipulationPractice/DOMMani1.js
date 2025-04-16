e21e7434773ccd15567a2cdb07c25be1908efd1e
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-16 09:24:55 +0400

    Finished hideShow function

--- DOMManipulationPractice/DOMMani1.js
+++ DOMManipulationPractice/DOMMani1.js
@@ -4,3 +4,12 @@ function changeHeading(){
 function changeDivBC(){
     document.getElementsByTagName("div")[0].style.backgroundColor = "blue";
 }
+function hideShow(){
+    let element = document.getElementsByTagName("p")[0]
+    if (getComputedStyle(element).visibility == "visible"){
+        element.style.visibility = "hidden";
+    }
+    else if(getComputedStyle(element).visibility == "hidden"){
+        element.style.visibility = "visible";
+    }
+}
