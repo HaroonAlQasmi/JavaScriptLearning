70c45ae440e37b0d9da6f0982c39094d53ee2491
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-16 10:25:34 +0400

    made a change button function

--- DOMManipulationPractice/DOMMani2.js
+++ DOMManipulationPractice/DOMMani2.js
@@ -1,3 +1,10 @@
 function changeTitle(){
     document.getElementsByTagName("title")[0].innerText = "A page abut lions";
 }
+function changeAlt(){
+    let img = document.getElementsByTagName("img")[0];
+    img.setAttribute("alt","there was supposed to be a lion looking man here");
+}
+function changeMe(){
+    document.getElementById("chB").innerText = "I am a changed button";
+}
