bca7892ca954f344f1f3531acc9e9f178ab68fff
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-16 11:55:26 +0400

    show/Hide text function was added

--- DOMManipulationPractice/DOMMani2.js
+++ DOMManipulationPractice/DOMMani2.js
@@ -47,3 +47,12 @@ function ilCC(id){
     let ilC = document.getElementById(id);
     ilC.style.backgroundColor = "brown";
 }
+function clickBoxShow(){
+    let inputType = document.getElementById("secret");
+    if(inputType.type === "password"){
+        inputType.type = "text";
+    }
+    else if(inputType.type === "text"){
+        inputType.type = "password";
+    }
+}
