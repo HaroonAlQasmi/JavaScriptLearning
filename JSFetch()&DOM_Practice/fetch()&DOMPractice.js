0270296d6cc85b709d7fa3c8629b06aba8d884b7
HaroonAlQasmi <hhhaaroonn13@gmail.com>
2025-04-17 10:29:11 +0400

    added a button to fetch posts

--- JSFetch()&DOM_Practice/fetch()&DOMPractice.js
+++ JSFetch()&DOM_Practice/fetch()&DOMPractice.js
@@ -3,19 +3,30 @@ function getUserData() {
         .then(response => response.json()) // Parse the response as JSON
         .then(users => {
             const container = document.getElementById('container');
-            container.innerHTML = ''; // Clear the container before adding new content
-
             users.forEach(user => {
-                const postDiv = document.createElement('div');
-                postDiv.innerHTML = `
+                const userDiv = document.createElement('div');
+                userDiv.innerHTML = `
                     <ul>
                     <li><h3>Name: ${user.name}</h3></li>
                     <p>Email: ${user.email}</p>
                     <p>Company: ${user.company.name}</p>
                     </ul>
                 `;
-                container.appendChild(postDiv); // Append the user data to the container
+                container.appendChild(userDiv); // Append the user data to the container
             });
         })
         .catch(error => console.error('Error fetching user data:', error));
+}
+function getPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts').then(response => response.json()).then(posts => {
+        const container = document.getElementById('container2');
+        posts.forEach(post => {
+            const postDiv = document.createElement('div');
+            postDiv.innerHTML = `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            `;
+            container.appendChild(postDiv);
+        });
+    })
 }
